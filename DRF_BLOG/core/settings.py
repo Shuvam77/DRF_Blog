1980a9532c2b17a3b664e9f9bfb2e9fe9abc589c
shuvam77 <shuvam77@gmail.com>
2022-09-27 14:06:03 +0200

    Simple_JWT installed and custom user model aaded

--- DRF_BLOG/core/settings.py
+++ DRF_BLOG/core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "corsheaders",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -147,3 +148,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (      
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
