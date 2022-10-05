41a6a9c59cbad69c59751e7d03575f07b1912c98
shuvam77 <shuvam77@gmail.com>
2022-10-05 20:17:30 +0200

    integrate openapi-schema

--- DRF_BLOG/core/urls.py
+++ DRF_BLOG/core/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -30,9 +31,12 @@ urlpatterns = [
 
     path('api/user/', include('users.urls', namespace='users')),
 
-
-
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    path('schema/', get_schema_view(
+        title = "Your Project",
+        description = "API for Django REST API.",
+        version="1.0.0"   
+    ), name="openapi-schema"),
 ]
